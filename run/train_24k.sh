530ac9933b45a32563b9ec3d4e862a7482025ae9
WoojinChung <58346392+gartland98@users.noreply.github.com>
2025-05-08 19:25:37 +0900

    Update train_24k.sh

--- run/train_24k.sh
+++ run/train_24k.sh
@@ -3,7 +3,7 @@
 export HF_HUB_ENABLE_HF_TRANSFER=1
 
 
-ACCELERATE_LOG_LEVEL=info TRANSFORMERS_VERBOSITY=info TRAINING_TYPE=PRETRAIN accelerate launch -m \
+ACCELERATE_LOG_LEVEL=info TRANSFORMERS_VERBOSITY=info TRAINING_TYPE=PROPOSED accelerate launch -m \
     --config_file accelerate/ds3.yaml \
     scripts.run_alignment \
     recipes/samples/24k.yaml
