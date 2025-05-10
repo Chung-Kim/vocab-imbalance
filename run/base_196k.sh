a9e6089446ca97b238ca3d2f0c7c2b0ab1955a84
WoojinChung <58346392+gartland98@users.noreply.github.com>
2025-05-10 17:25:55 +0900

    Add files via upload

---
+++ run/base_196k.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export HF_HUB_ENABLE_HF_TRANSFER=1
+
+
+ACCELERATE_LOG_LEVEL=info TRANSFORMERS_VERBOSITY=info TRAINING_TYPE=PRETRAIN accelerate launch -m \
+    scripts.run_alignment \
+    recipes/samples/196k.yaml
